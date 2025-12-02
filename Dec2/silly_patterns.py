731173a1f3ae171e1b6a6bfb6d5354de230fe384
Anna Marques <annh.scribner@gmail.com>
2025-12-02 07:04:17 -0800

    adding solution for day 2

---
+++ Dec2/silly_patterns.py
@@ -0,0 +1,38 @@
+def main():
+    save = []
+
+    with open('Dec2/ids.txt', 'r') as file:
+        line = file.readlines()
+        for l in line:
+            # print(l.split(','))
+            save = l.split(',')
+        
+    ranges = []
+    for s in save:
+        ranges.append(s.split('-'))
+        
+    sum_invalid_ids = 0
+
+    # going to each pair with start and end ranges
+    for i in range(len(ranges)):
+        start = int(ranges[i][0]) 
+        end = int(ranges[i][1])
+        
+        # if is_valid_range(len(str(end))): 
+            # go through the numbers from start to end
+        for num in range(start, end + 1):
+            # if size of num is even, do something. If not, ignore it
+            num_str = str(num)
+            if is_valid_range(len(num_str)):
+                half = len(num_str) // 2
+                first_half = num_str[:half]
+                last_half = num_str[half:]
+                if first_half == last_half:
+                    sum_invalid_ids += num
+
+    print(sum_invalid_ids)
+                
+def is_valid_range(num):
+    return num % 2 == 0 # if it is even returns true, otherwise it returns false
+
+main()
